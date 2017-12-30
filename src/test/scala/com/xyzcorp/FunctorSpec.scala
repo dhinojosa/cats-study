9f40c1b4e6874b4855fbeb8888462f2054a21df9
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-12-30 10:36:26 -0700

    added MIT License

--- src/test/scala/com/xyzcorp/FunctorSpec.scala
+++ src/test/scala/com/xyzcorp/FunctorSpec.scala
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2017 Daniel Hinojosa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 package com.xyzcorp
 
 import cats._
@@ -22,7 +44,9 @@ class FunctorSpec extends FunSuite with Matchers {
   }
 
   test("Functor for Future") {
-    val future = Future {40 * 10}
+    val future = Future {
+      40 * 10
+    }
     val result = Functor[Future].fmap(future)(x => x / 2)
     result.foreach(println)
     Await.ready(result, 3 seconds)
@@ -37,6 +61,6 @@ class FunctorSpec extends FunSuite with Matchers {
   test("Functor for Custom Objects") {
     val box = Box(100)
     val result = Functor[Box].fmap(box)(x => x + 100)
-    result should be (Box(200))
+    result should be(Box(200))
   }
 }
--- src/test/scala/com/xyzcorp/MonoidSpec.scala
+++ src/test/scala/com/xyzcorp/MonoidSpec.scala
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2017 Daniel Hinojosa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 package com.xyzcorp
 
 import cats._
@@ -22,13 +44,18 @@ class MonoidSpec extends FunSuite with Matchers {
 
   type CostType = AnyRef {def cost: Float}
 
-  implicit val basketMonoid: Monoid[Basket[CostType]] = new Monoid[Basket[CostType]] { //At compile time, this has to resolve
-    override def empty: Basket[CostType] = Basket[CostType]()
-    override def combine(x: Basket[CostType], y: Basket[CostType]): Basket[CostType] = Basket(x.items ++ y.items: _*)
-  }
+  implicit val basketMonoid: Monoid[Basket[CostType]] =
+    new Monoid[Basket[CostType]] { //At compile time, this has to resolve
+      override def empty: Basket[CostType] = Basket[CostType]()
+
+      override def combine
+      (x: Basket[CostType], y: Basket[CostType]): Basket[CostType] =
+        Basket(x.items ++ y.items: _*)
+    }
 
   test("Case 2: Using a custom monoid") {
-    val basketA = Basket(Egg("AA", .02f), Egg("A", .10f), Egg("A", .05f), Egg("AA", .04f))
+    val basketA = Basket(Egg("AA", .02f), Egg("A", .10f),
+      Egg("A", .05f), Egg("AA", .04f))
     val basketB = Basket(Egg("AAA", .01f), Egg("AA", .20f))
     val combined = Monoid[Basket[CostType]].combine(basketA, basketB)
     combined.count should be(6)
