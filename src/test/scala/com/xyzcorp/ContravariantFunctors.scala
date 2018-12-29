df2e1d5dabd2a25e6d98f3fc3851901a161eb1a5
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-12-29 13:08:31 -0700

    added the Contravariant and InvariantFunctors.scala

--- src/test/scala/com/xyzcorp/ContravariantFunctors.scala
+++ src/test/scala/com/xyzcorp/ContravariantFunctors.scala
@@ -1,15 +1,124 @@
 /*
  * Copyright 2018 Daniel Hinojosa
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package com.xyzcorp
 
-class ContravariantFunctors {
+import org.scalatest.{FunSpec, Matchers}
 
+class ContravariantFunctors extends FunSpec with Matchers {
+  describe("Contravariant Functors") {
+    it(
+      """represents prepending of an operation in a chain.
+        |  In other words, apply a function before creating
+        |  the type class.""".stripMargin) {
+
+      trait Printable[A] {
+        outer =>
+        def format(value: A): String
+
+        def contramap[B](func: B => A): Printable[B] = new Printable[B] {
+          override def format(value: B): String = {
+            outer.format(func(value))
+          }
+        }
+      }
+
+      def format[A](value: A)(implicit p: Printable[A]): String =
+        p.format(value)
+
+      implicit val stringPrintable: Printable[String] =
+        (value: String) => "\"" + value + "\""
+
+      implicit val cm: Printable[Int] =
+        stringPrintable.contramap((x: Int) => x.toString)
+
+      implicit val booleanPrintable: Printable[Boolean] =
+        (value: Boolean) => if (value) "yes" else "no"
+
+      format("Hello") should be("\"Hello\"")
+      format(true) should be("yes")
+      format(10) should be("\"10\"")
+    }
+
+    it("can be summoned by use of imports") {
+      import cats.Contravariant
+      import cats.Show
+      import cats.instances.string._
+
+      info("first we bring in the show typeclass")
+      val showString = Show[String]
+
+      info(
+        """We then use Contravariant[Show] with the show type class and
+          |  present a function that is to be applied before.""".stripMargin)
+      val showSymbol = Contravariant[Show].
+        contramap(showString)((sym: Symbol) => s"'${sym.name}")
+      showSymbol.show('dave) should be("'dave")
+    }
+
+    it("""can be summoned by use of imports, and this time with the use of
+        |  bringing one package that has everything needed""".stripMargin) {
+      import cats.Show
+      import cats.instances.string._
+      import cats.syntax.contravariant._
+
+      info("first we bring in the show typeclass")
+      val showString = Show[String]
+
+      info(
+        """We then use Contravariant[Show] with the show type class and
+          |  present a function that is to be applied before.""".stripMargin)
+
+      val showSymbol  = showString
+        .contramap((sym: Symbol) => s"'${sym.name}").show('dave)
+      showSymbol should be("'dave")
+    }
+
+    it (
+      """can be used to hitch onto another typeclass, say, a Monoid[A]
+        |  where the `combine` method does the following with Symbols
+        |  only providing a `Monoid` of `String`:
+        |
+        |  1. accept two Symbols as parameters;
+        |  2. convert the Symbols to Strings;
+        |  3. combine the Strings using Monoid[String];
+        |  4. convert the result back to a Symbol.
+        |
+        |  This can be implemented using `imap`""".stripMargin) {
+
+      import cats.Monoid
+      import cats.instances.string._ // for Monoid
+      import cats.syntax.invariant._ // for imap
+      import cats.syntax.semigroup._ // for |+|
+
+      implicit val symbolMonoid: Monoid[Symbol] =
+        Monoid[String].imap(Symbol.apply)(_.name)
+
+      info("establish the empty Monoid")
+      Monoid[String].empty
+
+      val result = 'hello |+| 'does |+| 'this |+| 'work
+      result shouldEqual Symbol("hellodoesthiswork")
+    }
+  }
 }
--- src/test/scala/com/xyzcorp/InvariantFunctors.scala
+++ src/test/scala/com/xyzcorp/InvariantFunctors.scala
@@ -1,5 +1,92 @@
+/*
+ * Copyright 2018 Daniel Hinojosa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.xyzcorp
 
-class InvariantFunctors {
+import org.scalatest.{FunSpec, Matchers}
 
+class InvariantFunctors extends FunSpec with Matchers {
+  describe("Invariant Functors") {
+    trait Codec[A] {
+      self =>
+      def encode(value: A): String
+
+      def decode(value: String): A
+
+      def imap[B](dec: A => B, enc: B => A): Codec[B] = new Codec[B] {
+        override def encode(value: B): String = self.encode(enc(value))
+
+        override def decode(value: String): B = dec(self.decode(value))
+      }
+    }
+
+    def encode[A](value: A)(implicit c: Codec[A]): String =
+      c.encode(value)
+
+    def decode[A](value: String)(implicit c: Codec[A]): A =
+      c.decode(value)
+
+    implicit val stringCodec: Codec[String] =
+      new Codec[String] {
+        def encode(value: String): String = value
+
+        def decode(value: String): String = value
+      }
+
+    implicit val intCodec: Codec[Int] =
+      stringCodec.imap(_.toInt, _.toString)
+
+    implicit val booleanCodec: Codec[Boolean] =
+      stringCodec.imap(_.toBoolean, _.toString)
+
+    implicit val doubleCodec: Codec[Double] =
+      stringCodec.imap(_.toDouble, _.toString)
+
+
+    it("""implements a method called imap that is informally
+         |  equivalent to a combination of map and contramap. imap generates
+         |  them via a pair of bidirectional transformations""".stripMargin) {
+      intCodec.encode(10) should be("10")
+      intCodec.decode("10") should be(10)
+      doubleCodec.encode(1230.00) should be("1230.0")
+    }
+
+    it("""can be used for custom types of course, done so this time, by
+         | using a method to extend where to find the codec.""".stripMargin) {
+
+      case class Box[A](value: A)
+      implicit def boxCodec[A](implicit c: Codec[A]): Codec[Box[A]] =
+        c.imap[Box[A]](Box(_), _.value)
+
+      val box = Box(40)
+
+      boxCodec[Int].encode(box) should be("40")
+
+      def foo[A](b:Box[A])(implicit c:Codec[Box[A]]): String = {
+        c.encode(b)
+      }
+
+      foo(box) should be ("40")
+    }
+  }
 }
