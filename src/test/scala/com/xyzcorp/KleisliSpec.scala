07af12821572ecae371c5820efc5fe8b9fd18901
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-05-20 13:44:32 -0600

    added Kleisli Specification

--- src/test/scala/com/xyzcorp/KleisliSpec.scala
+++ src/test/scala/com/xyzcorp/KleisliSpec.scala
@@ -22,8 +22,52 @@
 
 package com.xyzcorp
 
+import java.util.concurrent.Future
+
+import cats.data.Kleisli
 import org.scalatest.{FunSpec, Matchers}
 
-class KleisliSpec extends FunSpec with Matchers {
+import scala.concurrent.Future
 
+import scala.util.Try
+
+class KleisliSpec extends FunSpec with Matchers {
+   describe("Kleisli Data Type") {
+     import scala.language.higherKinds
+     it ("is a wrapper around the function A => F[B]") {
+       final case class MyKleisli[F[_], A, B](run: A => F[B])
+     }
+     it ("is meant for composition, take the following functions that can compose") {
+       val takeFirst: ((String, Int)) => String = t => t._1
+       val substring3: String => Try[String] = s => Try(s.substring(3))
+       val tryToSuccessOrEmpty: Try[String] => String = ts => ts.getOrElse("")
+       val stringCaps : String => String = _.toUpperCase
+
+       val f = takeFirst andThen substring3 andThen tryToSuccessOrEmpty andThen stringCaps
+       f("Consider", 90) should be ("CON")
+     }
+
+     it ("can be used to wrap various elements to one type called Kleisli so as to efficiently manipulate it") {
+
+       pending
+
+       import scala.language.higherKinds
+       import scala.concurrent.ExecutionContext.Implicits.global
+       import scala.concurrent.Future
+       type DB = String
+       val k1:Kleisli[Future, DB, String] = Kleisli { db =>
+         Future{"Hello"}
+       }
+       val k2:Kleisli[Option, DB, String] = Kleisli { db =>
+         Some("Reached optimal settings")
+       }
+       //TODO:Fix This
+//       import cats.implicits._
+//       val resultr = for {
+//         a <- k1
+//         b <- k2
+//       } yield (a + b)
+//       resultr
+     }
+   }
 }
