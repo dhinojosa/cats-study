58b60b06077e235fa724ebd598d13f3d3e6c2837
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-05-21 11:35:25 -0600

    Filled in more about Validated Spec

--- src/test/scala/com/xyzcorp/datatypes/ValidatedSpec.scala
+++ src/test/scala/com/xyzcorp/datatypes/ValidatedSpec.scala
@@ -160,16 +160,123 @@ class ValidatedSpec extends FunSpec with Matchers {
         case Left(FirstNameHasSpecialCharacters) => "first name special"
         case Right(y) => "everything correct"
       }
-      message should be ("password incorrect")
+      message should be("password incorrect")
     }
   }
 
-  describe("Validated Data Type") {
+  describe("Validated with use of a Chain or Non-Empty-Chain or Nec") {
     it(
-      """is a datatype that is will process validations
-        |  with returning hopefully helpful information
-        |  if it is not valid.""".stripMargin) {
-      pending
+      """can be moved towards a validated type but using an iteration to show
+        |  all the error messages, the problem is that Validated is
+        |  not a Monad, and therefore cannot be flatMapped nor can
+        |  it be placed inside of a for-comprehension""".stripMargin) {
+
+      info("First we import what is needed")
+
+      import cats.data._
+      import cats.data.Validated._
+      import cats.implicits._
+
+      info("Here we have the case class that is to be filled")
+
+      final case class RegistrationData(username: String, password: String,
+                                        firstName: String, lastName: String,
+                                        age: Int)
+
+      info(
+        "We then define our Validation objects again from the previous example")
+
+      sealed trait DomainValidation {
+        def errorMessage: String
+      }
+
+      case object UsernameHasSpecialCharacters extends DomainValidation {
+        def errorMessage: String = "Username cannot contain special characters."
+      }
+
+      case object PasswordDoesNotMeetCriteria extends DomainValidation {
+        def errorMessage: String = "Password must be at least 10 characters long, including an uppercase and a lowercase letter, one number and one special character."
+      }
+
+      case object FirstNameHasSpecialCharacters extends DomainValidation {
+        def errorMessage: String = "First name cannot contain spaces, numbers or special characters."
+      }
+
+      case object LastNameHasSpecialCharacters extends DomainValidation {
+        def errorMessage: String = "Last name cannot contain spaces, numbers or special characters."
+      }
+
+      case object AgeIsInvalid extends DomainValidation {
+        def errorMessage: String = "You must be aged 18 and not older than 75 to use our services."
+      }
+
+      info(
+        """Creating FormValidatorNec.  A Nec is a non-empty chain.
+          |  A chain is a collection much like a list but with better
+          |  performance in certain situations. See
+          |  com.xyzcorp.datatypes.ValidatedSpec and
+          |  https://typelevel.org/cats/datatypes/chain.html. A non-empty-chain
+          |  will ensure that we have at least one element""".stripMargin)
+
+      sealed trait FormValidatorNec {
+
+        info(
+          """Create a new type called ValidationResult, that will
+            |  always use the DomainValidation object for the
+            |  evil side, This will make it a * -> *""".stripMargin)
+
+        type ValidationResult[A] = ValidatedNec[DomainValidation, A]
+
+        info(
+          """validNec is a implicit wrapper that raises any
+            |  object as a valid or an invalid non-empty-chain""".stripMargin)
+
+        private def validateUserName(userName: String): ValidationResult[String] =
+          if (userName.matches("^[a-zA-Z0-9]+$")) userName
+            .validNec else UsernameHasSpecialCharacters.invalidNec
+
+        private def validatePassword(password: String): ValidationResult[String] =
+          if (password.matches(
+            "(?=^.{10,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$")) password
+            .validNec
+          else PasswordDoesNotMeetCriteria.invalidNec
+
+        private def validateFirstName(firstName: String): ValidationResult[String] =
+          if (firstName.matches("^[a-zA-Z]+$")) firstName
+            .validNec else FirstNameHasSpecialCharacters.invalidNec
+
+        private def validateLastName(lastName: String): ValidationResult[String] =
+          if (lastName.matches("^[a-zA-Z]+$")) lastName
+            .validNec else LastNameHasSpecialCharacters.invalidNec
+
+        private def validateAge(age: Int): ValidationResult[Int] =
+          if (age >= 18 && age <= 75) age.validNec else AgeIsInvalid.invalidNec
+
+        def validateForm(username: String, password: String, firstName: String,
+                         lastName: String,
+                         age: Int): ValidationResult[RegistrationData] = {
+          (validateUserName(username),
+            validatePassword(password),
+            validateFirstName(firstName),
+            validateLastName(lastName),
+            validateAge(age)).mapN(RegistrationData)
+        }
+      }
+
+      info("Create a singleton object from the FormValidatorNec trait")
+
+      object FormValidatorNec extends FormValidatorNec
+
+      info("Now we can run our validation")
+      val result: FormValidatorNec.ValidationResult[RegistrationData] = FormValidatorNec
+        .validateForm(
+          username = "Joe%%%",
+          password = "password",
+          firstName = "John",
+          lastName = "Doe",
+          age = 21
+        )
+      result.isInvalid should be (true)
     }
   }
 }
